c62a80a7abbc90c92d1af743678c31e49b83d59c
Hisham Khaled <hishamk1999@gmail.com>
2024-10-11 02:19:04 +0300

    first commmit init.

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.sass';
+
+function App() {
+  return (
+    <>
+      <table>
+        {/* Table header */}
+        <thead></thead>
+        {/* Table body */}
+        <tbody></tbody>
+        {/* Table footer */}
+        <tfoot></tfoot>
+      </table>
+    </>
+  );
+}
+
+export default App;
